5ab200a9bd23101506a11fe88bc34e5a8f810174
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-10-05 21:10:07 +0500

    Week 5 JS Practice

---
+++ Week5/BookStore/index.js
@@ -0,0 +1,30 @@
+function addBook() {
+    const book = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+
+    const div = document.getElementById('books');
+    const bookEntry = document.createElement('div');
+    bookEntry.classList.add('book-entry'); 
+    const bookelement = document.createElement('p');
+    bookelement.textContent = 'Book: ' + book;
+
+    const authorelement = document.createElement('p');
+    authorelement.textContent = 'Author: ' + author;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+
+    removeButton.addEventListener('click', function () {
+        div.removeChild(bookEntry); 
+    });
+
+    bookEntry.appendChild(bookelement);
+    bookEntry.appendChild(authorelement);
+    bookEntry.appendChild(removeButton);
+
+    div.appendChild(bookEntry);
+
+    // Clear input fields
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+}
